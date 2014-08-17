fff599f54280016c2872bdbcc9d54918533c1f3f
Joakin <joaquin@chimeces.com>
2014-08-17 14:30:58 +0200

    Add react router

--- browser.js
+++ browser.js
@@ -3,15 +3,59 @@ console.log('I\'m in the browser!');
 
 var fs = require('fs');
 var React = require('react');
-var App = require('./browser/ui/app.jsx');
+var Routes = require('react-router/Routes');
+var Route = require('react-router/Route');
+var Link = require('react-router/Link');
+
+
+// var App = require('./browser/ui/app.jsx');
 var fonts = require('./fonts.json');
 
 var sampleText = fs.readFileSync(__dirname + '/static/sample.txt', 'utf8');
 
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="about">About</Link></li>
+          <li><Link to="home">Home</Link></li>
+        </ul>
+        <this.props.activeRouteHandler/>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
+  render: function() {
+    return <h2>Home</h2>;
+  }
+});
+
+var About = React.createClass({
+  render: function() {
+    return <h2>About</h2>;
+  }
+});
+
+
 React.renderComponent(
-  App({
-    fonts: fonts,
-    text: sampleText
-  }),
+  Routes({ location:"history" },
+    Route({ handler: App },
+      Route({ name: 'home', handler: Home }),
+      Route({ name: 'about', handler: About })
+    )
+  ),
   document.body
 );
+
+// React.renderComponent(
+//   App({
+//     fonts: fonts,
+//     text: sampleText
+//   }),
+//   document.body
+// );
+
